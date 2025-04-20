bd3370120eb90358405f5f4c0f05576ca210609c
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-04-20 18:09:21 +0300

    first commit

---
+++ assets/arrow-big-right-dash-C5qwJRte.js
@@ -0,0 +1,6 @@
+import{c as o}from"./createLucideIcon-B_PIV35T.js";import"./index-BsfaHZDE.js";/**
+ * @license lucide-react v0.501.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M5 9v6",key:"158jrl"}],["path",{d:"M9 9h3V5l7 7-7 7v-4H9V9z",key:"1sg2xn"}]],e=o("arrow-big-right-dash",r);export{r as __iconNode,e as default};
